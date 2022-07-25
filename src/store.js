8a6695408d27a7ba0530b4de53adcfe24630b3cd
River <swimmingr@gmail.com>
2022-07-26 00:32:56 +0900

    강의내용 필기 일부

--- src/store.js
+++ src/store.js
@@ -19,7 +19,7 @@ export const deleteToDo=id=>{
 const reducer = (state=[],action)=>{
     switch(action.type){
         case ADD: return [{text: action.text,id:Date.now()},...state];
-        case DELETE:return state.filter(toDo=>toDo !== action.id);
+        case DELETE:return state.filter(toDo=>toDo     !== action.id);
         default: return state;
     }
 }
